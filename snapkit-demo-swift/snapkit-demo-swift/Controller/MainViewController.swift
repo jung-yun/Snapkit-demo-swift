49e6c16c947bd9953fb5373e6d6b76ff01c8b270
jung-yun <tgms021@gmail.com>
2022-04-26 13:47:30 +0900

    Add code demonstrating SnapKit usage

--- snapkit-demo-swift/snapkit-demo-swift/Controller/MainViewController.swift
+++ snapkit-demo-swift/snapkit-demo-swift/Controller/MainViewController.swift
@@ -10,71 +10,167 @@ import SnapKit
 
 class MainViewController: UIViewController {
 
-    private let biggerWindow: UIView = {
+    //MARK: - Properties
+    
+    /// The outer view containing the middleView
+    private let biggerView: UIView = {
         let view = UIView()
         view.backgroundColor = .systemBlue
         return view
     }()
     
-    private let middleWindow: UIView = {
+    /// Inner view of the biggerView. It initially contains the smaller view
+    private let middleView: UIView = {
         let view = UIView()
         view.backgroundColor = .systemGreen
         return view
     }()
     
-    private let smallerWindow: UIView = {
+    /// Inner view of the middleView. Is going to change its layout and parent view in this project
+    private let smallerView: UIView = {
         let view = UIView()
         view.backgroundColor = .systemRed
         return view
     }()
     
+    /// change layouts of the smaller view
     private let layoutChangeButton: UIButton = {
         let button = UIButton()
         button.backgroundColor = .systemCyan
-        button.setTitle("change red window's auto layout", for: .normal)
+        button.setTitle("Switch red view's auto layout", for: .normal)
+        button.titleLabel?.lineBreakMode = .byWordWrapping
+        button.titleLabel?.numberOfLines = 0
         return button
     }()
     
+    
+    /// change parent view of the smaller view
+    private let parentChangeButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemPink
+        button.setTitle("Switch parent of red view", for: .normal)
+        button.titleLabel?.lineBreakMode = .byWordWrapping
+        button.titleLabel?.numberOfLines = 0
+        return button
+    }()
+    
+    private var smallerViewLayoutChanged: Bool = false
+    private var smallerViewParentChanged: Bool = false
+    
+    //MARK: - Callbacks
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.addSubview(biggerWindow)
-        self.biggerWindow.addSubview(middleWindow)
-        self.middleWindow.addSubview(smallerWindow)
+        self.view.addSubview(biggerView)
+        self.biggerView.addSubview(middleView)
+        self.middleView.addSubview(smallerView)
         self.view.addSubview(layoutChangeButton)
+        self.view.addSubview(parentChangeButton)
         
-        biggerWindow.snp.makeConstraints { make in
+        self.setBiggerViewLayout()
+        self.setMiddleViewLayout()
+        self.setSmallerViewLayout()
+        self.setLayoutChangeButtonLayout()
+        self.configureParentChangeButton()
+        
+        self.setLayoutChangeButtonAction()
+        self.setParentChangeButtonAction()
+    }
+    
+    //MARK: - Methods
+    private func setBiggerViewLayout() {
+        biggerView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
-        
-        middleWindow.snp.makeConstraints { make in
+    }
+    
+    private func setMiddleViewLayout() {
+        middleView.snp.makeConstraints { make in
             make.leading.trailing.equalToSuperview()
-            make.top.equalToSuperview().inset(100)
-            make.bottom.equalToSuperview().inset(100)
+            make.top.equalToSuperview().inset(300)
+            make.bottom.equalToSuperview().inset(300)
         }
-        
-        smallerWindow.snp.makeConstraints { make in
+    }
+    
+    private func setSmallerViewLayout() {
+        smallerView.snp.makeConstraints { make in
             make.center.equalToSuperview()
             make.size.equalToSuperview().dividedBy(10)
         }
-        
+    }
+    
+    private func setLayoutChangeButtonLayout() {
         layoutChangeButton.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
-            make.centerY.equalTo(self.view.snp_bottomMargin).inset(20)
+            make.bottom.equalTo(self.view.snp.bottom).inset(20)
+            make.width.equalToSuperview().dividedBy(1.2)
+            make.height.equalToSuperview().dividedBy(10)
         }
-        
-        layoutChangeButton.addAction(UIAction(handler: { [unowned self] touched in
-            UIView.animate(withDuration: 5, delay: 0.5, options: .transitionCurlUp) {
-                self.smallerWindow.snp.remakeConstraints { make in
-                    make.bottom.equalTo(self.middleWindow.snp_bottomMargin)
+    }
+    
+    private func setLayoutChangeButtonAction() {
+        layoutChangeButton.addAction(UIAction(handler: { [unowned self] _ in
+            self.remakeSmallerViewLayout()
+        }), for: .touchUpInside)
+    }
+    
+    private func configureParentChangeButton() {
+        parentChangeButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalTo(layoutChangeButton.snp.top).inset(-20)
+            make.width.equalToSuperview().dividedBy(1.2)
+            make.height.equalToSuperview().dividedBy(10)
+        }
+    }
+    
+    private func setParentChangeButtonAction() {
+        parentChangeButton.addAction(UIAction(handler: { [unowned self] _ in
+            self.resetSmallerViewParent()
+        }), for: .touchUpInside)
+    }
+    
+    private func remakeSmallerViewLayout() {
+        UIView.animate(withDuration: 0.5, delay: 0.0, options: .transitionCurlUp) {
+            if self.smallerViewLayoutChanged {
+                /// place the smaller window to center of the parent view
+                /// notice you use `remakeContraints` method
+                self.smallerView.snp.remakeConstraints { make in
+                    make.center.equalToSuperview()
+                    make.size.equalToSuperview().dividedBy(10)
+                }
+                
+            } else {
+                /// palce the smaller window to the upper-left of the parent view
+                self.smallerView.snp.remakeConstraints { make in
+                    make.top.equalToSuperview().inset(20)
+                    make.leading.equalToSuperview().inset(20)
                     make.size.equalToSuperview().dividedBy(20)
                 }
-                self.view.layoutIfNeeded()
-            } completion: { _ in
-                // to do
             }
-        }), for: .touchUpInside)
+            
+            self.view.layoutIfNeeded()
+        } completion: { _ in
+            self.smallerViewLayoutChanged = !self.smallerViewLayoutChanged
+            print("Changed auto layout successfully")
+        }
+    }
+    
+    private func resetSmallerViewParent() {
+        self.smallerView.removeFromSuperview()
         
+        /// switch parent of smaller view between the middleView and biggerView
+        if self.smallerViewParentChanged {
+            middleView.addSubview(smallerView)
+        } else {
+            biggerView.addSubview(smallerView)
+        }
+        
+        /// notice that you can re-use the same auto layout code here again.
+        /// That is because SnapKit does not refer to the parent view explicitly, making it possible to automatically adapting auto layout to a new parent view
+        self.setSmallerViewLayout()
+        self.smallerViewParentChanged = !self.smallerViewParentChanged
+        self.view.layoutIfNeeded()
+        print("Changed parent of smaller view successfully")
     }
     
 }
